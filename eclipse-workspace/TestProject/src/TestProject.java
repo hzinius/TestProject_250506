1f44028c135d5762461d8fdb5327aece78677baa
h.zi.nius@gmail.com <h.zi.nius@gmail.com>
2024-12-10 22:28:27 +0900

    test 2

--- eclipse-workspace/TestProject/src/TestProject.java
+++ eclipse-workspace/TestProject/src/TestProject.java
@@ -4,6 +4,7 @@ public class TestProject {
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
+		System.out.println("Hello2");
 		/*
 		// p.65 더하기 예
 		Scanner input = new Scanner(System.in);
