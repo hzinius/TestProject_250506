13c76c0d90044e60be98186fdcdeeb3e7763533f
h.zi.nius@gmail.com <h.zi.nius@gmail.com>
2024-12-10 22:31:03 +0900

    test 1031

--- eclipse-workspace/TestProject/src/TestProject.java
+++ eclipse-workspace/TestProject/src/TestProject.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class TestProject {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("Hello1");
 		System.out.println("Hello2");
 		
 		// p.65 더하기 예
