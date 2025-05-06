38f9f7c00791d6e30cece2deacbc58fb789e3dcb
hzinius <123353211+hzinius@users.noreply.github.com>
2025-05-06 18:02:37 +0900

    Update Main.java

--- src/com/test/java/Main.java
+++ src/com/test/java/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello123");
-		System.out.println("Hi123");
+		System.out.println("Hi123456");
 	}
 
 }
